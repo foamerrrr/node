fb6ebdbcd1f297f89026577c01815783f3501c5a
foamerrrr <laurentchhou3@gmail.com>
2026-06-23 12:13:33 +0200

    act 8

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import "dotenv/config";
 import express from "express";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
+import router from "./app/routes/index.js";
+ 
 const app = express();
-
+ 
 const databaseUrl = new URL(process.env.DATABASE_URL);
 const adapter = new PrismaMariaDb({
   host: databaseUrl.hostname,
@@ -14,10 +15,12 @@ const adapter = new PrismaMariaDb({
   database: databaseUrl.pathname.replace("/", ""),
 });
 export const prisma = new PrismaClient({ adapter });
-
+ 
 prisma
   .$connect()
   .then(() => console.log("Database connected..."))
   .catch((err) => console.log(err));
-
+ 
+app.use("/api", router);
+ 
 export default app;
--- prisma/migrations/20260622133337_init/migration.sql
+++ prisma/migrations/20260622133337_init/migration.sql
@@ -5,6 +5,8 @@ CREATE TABLE `User` (
     `lastName` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -14,8 +16,8 @@ CREATE TABLE `User` (
 CREATE TABLE `Wood` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `type` ENUM('softwood', 'exotic_wood', 'noble_and_hardwoods') NOT NULL,
-    `hardness` ENUM('tender', 'medium_hard', 'hard') NOT NULL,
+    `type` ENUM('softwood', 'exotic wood', 'noble and hardwoods') NOT NULL,
+    `hardness` ENUM('tender', 'medium hard', 'hard') NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
