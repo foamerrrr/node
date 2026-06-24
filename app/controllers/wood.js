42b7bdcd9682489a7082b1ca8c82a75b9a692515
foamerrrr <laurentchhou3@gmail.com>
2026-06-24 11:27:16 +0200

    push 14 & 15

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -8,3 +8,16 @@ export const readAll = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+export const readByHardness = async (req, res) => {
+  try {
+    const woods = await prisma.wood.findMany({
+      where: {
+        hardness: req.params.hardness,
+      },
+    });
+    res.json(woods);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- app/routes/wood.js
+++ app/routes/wood.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import * as woodController from '../controllers/wood.js';
 
 router.get('/', woodController.readAll);
+router.get('/:hardness', woodController.readByHardness);
 
 export default router;
