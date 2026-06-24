5d82a7428958be4a3b339b142af388ea55f96763
foamerrrr <laurentchhou3@gmail.com>
2026-06-24 14:07:55 +0200

    act 16

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import { prisma } from '../../app.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
   try {
@@ -22,6 +23,51 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send('You are logged in');
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Email ou mot de passe incorrect."
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Email ou mot de passe incorrect."
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return res.status(200).json({
+      token,
+      user: userWithoutPassword,
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message || "Some error occured while logging in",
+    });
+  }
 };
