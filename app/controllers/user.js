ce4f10ac9b479f4cb66037cc7ab3df17cad5ada2
foamerrrr <laurentchhou3@gmail.com>
2026-06-24 10:15:17 +0200

    act 12 & 13

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,27 @@
-export const signup = (req, res) => {
-  res.send('You are signed up');
+import bcrypt from 'bcrypt';
+import { prisma } from '../../app.js';
+
+export const signup = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({
+      name: error.name,
+      message: error.message,
+      clientVersion: error.clientVersion,
+    });
+  }
 };
 
 export const login = (req, res) => {
